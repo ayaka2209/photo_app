8d836c8a037b7a1111669831dddf47f9348cb3ed
ayaka tooya <na081543@icloud.com>
2022-10-16 15:40:12 +0900

    エラーページ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,5 +46,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
-
+  config.consider_all_requests_local = false
 end
